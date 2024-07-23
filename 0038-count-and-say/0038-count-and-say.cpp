0da1a36325e79805154ea5d6c8e5fc34ef2a083d
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2024-07-23 23:58:49 +0530

    Time: 5 ms (48.6%), Space: 13.1 MB (14.8%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string countNsay(int n, char c){
+        // cout<<n<<" "<<c<<endl;
+        return to_string(n)+c;
+    }
+    string rel(string s){
+        int n = s.size(),c=0,i=0;
+        string ans = "";
+        char a = s[0];
+        while(i<n){
+            if(a==s[i]){
+                c++;
+            }
+            else{
+                ans += countNsay(c,a);
+                a = s[i];
+                c=1;
+            }
+            i++;
+        }
+        ans += countNsay(c,a);
+        // cout<<n<<" "<<c<<endl;
+        // cout<<ans<<endl;
+        return ans;
+    }
+    string countAndSay(int n) {
+        vector<string> res(31,"");
+        if(n==1) return "1";
+        // cout<<rel[1]<<endl;
+        if(res[n]=="") {
+            res[n] = rel(countAndSay(n-1));
+        }
+        return res[n];
+    }
+};
