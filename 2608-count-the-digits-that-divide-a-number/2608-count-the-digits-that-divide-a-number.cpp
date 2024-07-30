1b686ad28077fed59dd1b7ba87d3ee36fbd2b60c
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2024-07-30 21:50:28 +0530

    Time: 2 ms (54.93%), Space: 7.3 MB (23.33%) - LeetHub

---
+++ 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int num1 = num, ans = 0;
+        while(num1>0){
+            if(num%(num1%10)==0)
+                ans++;
+            num1 /= 10;
+        }
+        return ans;
+    }
+};
