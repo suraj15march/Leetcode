82696351293d3e2278f54a8a0508ebe716e0a5f5
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2025-01-26 00:14:54 +0530

    Time: 3 ms (69.55%), Space: 15.7 MB (48.92%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    void func(int left, int right, string ans, vector<string>&res){
-        if(left==0 && right == 0)
-            {
-                res.push_back(ans);
-            }
-        if(left>0){
-            // ans += '(';
-            func(left-1, right, ans+"(", res);
+    void func(int left, int right, vector<string>&ans, string res){
+        if(left==0 && right == 0){
+            ans.push_back(res);
         }
-        if(left<right && right>0){
-            // ans += ')';
-            func(left, right-1, ans+")", res);
+        if(left>0){
+            func(left-1, right, ans, res+'(');
+        }
+        if(right>0 && left<right){
+            func(left, right-1, ans, res+')');
         }
     }
     vector<string> generateParenthesis(int n) {
-        int left = n, right = n;
-        vector<string> res;
-        string ans = "";
-        func(left, right, ans, res);
-        return res;
+        vector<string> ans;
+        string res = "";
+        func(n, n, ans, res);
+        return ans;
     }
 };
