95cef5eaf0a2edd2f80f5d7793eb112a6e416fb0
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2025-01-27 23:41:15 +0530

    Time: 258 ms (12.27%), Space: 139.9 MB (15.51%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void backtrack(vector<vector<int>>&ans, vector<int>list, int n, int k, int index){
+        if(list.size()==k){
+            ans.push_back(list);
+            return;
+        }
+        for(int i=index; i<=n; i++){
+            list.push_back(i);
+            backtrack(ans, list, n, k, index+1);
+            index++;
+            list.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>ans;
+        vector<int>list;
+        backtrack(ans, list, n, k, 1);
+        return ans;
+    }
+};
