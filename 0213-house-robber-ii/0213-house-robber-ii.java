d7629b6b6f6dbce9c80bb0d3bb80610cd798a120
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2025-09-27 23:25:12 +0530

    Time: 0 ms (100%), Space: 41.4 MB (18.87%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    private int[] ans;
+    private int func(int[] nums, int index, int n){
+        if(index>=n) return 0;
+        if(ans[index] != -1) return ans[index];
+        ans[index] = Math.max(nums[index]+func(nums, index+2, n), func(nums,index+1, n));
+        return ans[index];
+    }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==1) return nums[0];
+        ans = new int[n];
+        Arrays.fill(ans, -1);
+        int result1 = func(nums, 0, n-1);
+        ans = new int[n];
+        Arrays.fill(ans, -1);
+        int result2 = func(nums, 1, n);
+        return Math.max(result1, result2);
+    }
+}
