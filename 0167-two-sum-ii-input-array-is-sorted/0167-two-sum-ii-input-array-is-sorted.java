19d0adbfe911540f61baa8d7d0638ba414c6ddd7
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-06-05 22:21:25 +0530

    Time: 2 ms (96.38%), Space: 48.5 MB (40.81%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,26 +3,31 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[2];
 
-        // int i = 0, j = n-1;
-        // while(i<j){
-        //     if(nums[i] + nums[j] == target){
-        //         ans[0] = i+1;
-        //         ans[1] = j+1;
-        //         return ans;
-        //     }
-
-        // }
-
-        for (int i=0; i<n; i++){
-            int temp = target - nums[i];
-            for(int j=i+1; j<n; j++){
-                if(temp == nums[j]){
-                    ans[0] = i+1;
-                    ans[1] = j+1;
-                    return ans;
-                }
+        int i = 0, j = n-1;
+        while(i<j){
+            if(nums[i] + nums[j] == target){
+                ans[0] = i+1;
+                ans[1] = j+1;
+                return ans;
+            }
+            else if(nums[i] + nums[j] > target){
+                j--;
+            }
+            else{
+                i++;
             }
         }
+
+        // for (int i=0; i<n; i++){
+        //     int temp = target - nums[i];
+        //     for(int j=i+1; j<n; j++){
+        //         if(temp == nums[j]){
+        //             ans[0] = i+1;
+        //             ans[1] = j+1;
+        //             return ans;
+        //         }
+        //     }
+        // }
         return ans;
     }
 }
