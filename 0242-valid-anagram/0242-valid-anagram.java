db033e7051a073723f5127c2d20148feeaae6f40
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-06-07 09:59:40 +0530

    Time: 5 ms (62.55%), Space: 44.2 MB (89.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,8 +5,6 @@ class Solution {
         int[] arr = new int[26];
         for(int i=0; i<n; i++){
             arr[s.charAt(i)-'a']++;
-        }
-        for(int i=0; i<m; i++){
             arr[t.charAt(i)-'a']--;
         }
         for(int i=0; i<26; i++){
