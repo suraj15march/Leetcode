6304edc39d1b8f95a44e5bc2e03f86fca6eb1508
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-06-11 00:31:48 +0530

    Time: 9 ms (91.71%), Space: 46.7 MB (69.86%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,26 +1,21 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        int n = s.length(), m = t.length();
-        if(n!=m) return false;
-        int[] arr = new int[26];
-        for(int i=0; i<n; i++){
-            arr[s.charAt(i)-'a']++;
-            arr[t.charAt(i)-'a']--;
-        }
-        for(int i=0; i<26; i++){
-            if(arr[i]>0) return false;
-        }
-        return true;
-    }
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> res = new ArrayList<>();
-        int n = s.length(), m = p.length();
-        StringBuilder str = new StringBuilder(s);
-        for(int i=0; i+m<=n; i++){
-            if(isAnagram(str.substring(i,i+m),p)){
-                res.add(i);
+        int[] l1 = new int[26];
+        int[] l2 = new int[26];
+        int m = s.length(), n = p.length();
+        List<Integer> ans = new ArrayList<>();
+        if(n>m) return ans;
+        for(int i=0; i<n; i++) l2[p.charAt(i)-'a']++;
+        for(int i=0; i<m; i++){
+            if(i>=n){
+                if(Arrays.equals(l1,l2))
+                    ans.add(i-n);
+                l1[s.charAt(i-n)-'a']--;
             }
+            l1[s.charAt(i)-'a']++;
         }
-        return res;
+        if(Arrays.equals(l1,l2))
+            ans.add(m-n);
+        return ans;
     }
 }
