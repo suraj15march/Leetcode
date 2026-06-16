ffc033825d91c79168c325724729fc93b3b2da2c
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-06-16 18:00:56 +0530

    Time: 3 ms (100%), Space: 55.4 MB (48.53%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -1,39 +1,14 @@
 class Solution {
-    public void duplicate(Stack<Character> st){
-        StringBuilder str = new StringBuilder();
-        while(!st.empty()){
-            str.append(st.peek());
-            st.pop();
-        }
-        str.append(str);
-        str.reverse();
-        for(char ch: str.toString().toCharArray())   st.push(ch);
-    }
-    public void reverse(Stack<Character> st){
-        StringBuilder str = new StringBuilder();
-        while(!st.empty()){
-            str.append(st.peek());
-            st.pop();
-        }
-        for(char ch: str.toString().toCharArray())   st.push(ch);
-    }
-
     public String processStr(String s) {
-        StringBuilder ans = new StringBuilder();
-        Stack<Character> st = new Stack<>();
-        for(char ch: s.toCharArray()){
-            switch(ch){
-                case '*': if(!st.empty()) st.pop(); break;
-                case '#': duplicate(st); break;
-                case '%': reverse(st); break;
-                default: st.push(ch);
+        StringBuilder sb = new StringBuilder();
+        for(char c: s.toCharArray()){
+            switch(c){
+                case '*': if(sb.length()>0) sb.deleteCharAt(sb.length()-1); break;
+                case '#': sb.append(sb.toString()); break;
+                case '%': sb.reverse(); break;
+                default: sb.append(c);
             }
         }
-        while(!st.empty()){
-            ans.append(st.peek());
-            st.pop();
-        }
-        ans.reverse();
-        return ans.toString();
+        return sb.toString();
     }
 }
