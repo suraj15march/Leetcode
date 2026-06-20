01e8c0799506ffceda4b5c3a35f467c16230160f
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-06-20 10:10:17 +0530

    Time: 1 ms (97.92%), Space: 42.7 MB (22.71%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -2,13 +2,19 @@ class Solution {
     public int mySqrt(int x) {
         if(x == 0 || x == 1) return x;
         int start = 1, end = x/2;
+        int ans = 0;
         while(start<=end){
             int mid = start+(end-start)/2;
             long product = (long)mid*mid;
             if(product == x) return mid;
-            else if(x < product) end = mid-1;
-            else start = mid+1;
+            else if(x < product) {
+                end = mid-1;
+            }
+            else {
+                ans = mid;
+                start = mid+1;
+            }
         }
-        return start-1;
+        return ans;
     }
 }
