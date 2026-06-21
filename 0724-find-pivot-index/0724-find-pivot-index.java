e57aecdf3ca358823338664af798ef2d1c560289
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-06-21 19:04:05 +0530

    Time: 1 ms (98.55%), Space: 47.4 MB (39.67%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,16 +1,10 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int n = nums.length;
-        int[] prefix = new int[n];
-        prefix[0] = nums[0];
-        for(int i=1; i<n; i++){
-            prefix[i] = prefix[i-1] + nums[i];
-        }
-        int sum = prefix[n-1];
-        if(prefix[0]==sum) return 0;
-        for(int i=1; i<n; i++){
-            if(prefix[i-1] == sum-prefix[i])
-                return i;
+        int sum = 0, s = 0;
+        for(int i:nums) sum += i;
+        for(int i=0; i<nums.length; i++){
+            if(2*s+nums[i] == sum) return i;
+            s += nums[i];
         }
         return -1;
     }
