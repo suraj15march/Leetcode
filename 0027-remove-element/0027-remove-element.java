70149fca94e76ac8f53a8dd969ee4b68e2d741d4
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-06-24 21:03:53 +0530

    Time: 0 ms (100%), Space: 43.5 MB (42.17%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,21 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int left = 0, right = 0, n = nums.length;
-        while(left <= right && right<n){
-            if(nums[left] == val && nums[right]!=val){
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                left++;
+        int l = 0, h = 0;
+        while(h<nums.length){
+            if(nums[h] != val){
+                nums[l] = nums[h];
+                l++;
             }
-            else if(nums[left] != val && nums[right]!=val){
-                left++;
-                right++;
-            }
-            else if(nums[left] == val && nums[right]==val){
-                right++;
-            }
+            h++;
         }
-        return left;
+        return l;
     }
 }
