1ab57d5e25f0452ec705f2b3a1b8818de0351003
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-06-28 19:01:31 +0530

    Time: 48 ms (99.72%), Space: 148.9 MB (48.83%) - LeetHub

--- 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.java
+++ 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.java
@@ -3,8 +3,12 @@ class Solution {
         Arrays.sort(nums);
         long ans = 0;
         for(int i= nums.length-1; i>= nums.length-k; i--){
-            if(mul>1) ans += (long) nums[i]*mul;
-            else ans += nums[i];
+            if(mul>1){
+                ans += (long) nums[i]*mul;
+            }
+            else{
+                ans += nums[i];
+            }
             mul--;
         }
         return ans;
