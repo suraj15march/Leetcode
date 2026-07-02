020fcc2ec84f52b50622a75757ad46852393ec08
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-07-03 00:41:06 +0530

    Time: 1 ms (86.73%), Space: 44.6 MB (56.74%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,23 +1,24 @@
 class Solution {
-    public void func(List<String>ans, StringBuffer sb, int n, int open, int close){
+    private void helper(List<String>ans, StringBuilder sb, int open, int close, int n){
         if(sb.length() == 2*n){
-            ans.add(new String(sb));
+            ans.add(new StringBuilder(sb).toString());
             return;
         }
+        // if(open>n || close>n) return;
         if(open<n){
             sb.append('(');
-            func(ans, sb, n, open+1, close);
+            helper(ans, sb, open+1, close, n);
             sb.deleteCharAt(sb.length()-1);
         }
         if(close<open){
             sb.append(')');
-            func(ans, sb, n, open, close+1);
+            helper(ans, sb, open, close+1, n);
             sb.deleteCharAt(sb.length()-1);
         }
     }
     public List<String> generateParenthesis(int n) {
-        List<String>ans = new ArrayList<>();
-        func(ans, new StringBuffer(), n, 0, 0);
+        List<String>ans = new ArrayList();
+        helper(ans, new StringBuilder(), 0, 0, n);
         return ans;
     }
 }
