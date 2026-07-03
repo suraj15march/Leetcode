d49bad41ea001338ce6175a283f00831999c445e
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-07-03 21:55:34 +0530

    Time: 17 ms (23.52%), Space: 48.4 MB (59.23%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -4,7 +4,7 @@ class Solution {
         int[] ans = new int[n];
         Stack<Integer> stack = new Stack();
         stack.push(nums[0]);
-        for(int i=2*n-1; i>=0; i--){
+        for(int i=2*n-2; i>=0; i--){
             while(!stack.isEmpty() && nums[i%n]>=stack.peek()) stack.pop();
             if(!stack.isEmpty()) ans[i%n] = stack.peek();
             else ans[i%n] = -1;
