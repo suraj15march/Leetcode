37f1c02a177802ec97366b4698123dcdfa96306a
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-07-05 23:31:15 +0530

    Time: 0 ms (100%), Space: 43.9 MB (92.81%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -5,16 +5,24 @@ class Solution {
         List<String>ans = new ArrayList();
         String push = new String("Push");
         String pop = new String("Pop");
-        int j=0;
-        for(int i=1; i<=n && j<target.length; i++){
-            stack.push(i);
+        int i=1, j=0;
+        // for(int i=1; i<=n && j<target.length; i++){
+        //     stack.push(i);
+        //     ans.add(push);
+        //     if(stack.peek() != target[j]){
+        //         stack.pop();
+        //         ans.add(pop);
+        //     }
+        //     else j++;
+        // }
+        do{
             ans.add(push);
-            if(stack.peek() != target[j]){
-                stack.pop();
+            if(i != target[j]){
                 ans.add(pop);
             }
             else j++;
-        }
+            i++;
+        }while(i<=n && j<target.length);
         return ans;
     }
 }
