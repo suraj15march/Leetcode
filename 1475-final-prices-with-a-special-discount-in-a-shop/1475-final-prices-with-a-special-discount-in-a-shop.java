77fd45c3832c89ad6102d63910caaa696e99a93b
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-07-06 18:46:42 +0530

    Time: 1 ms (99.55%), Space: 45.1 MB (75.22%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,19 +1,40 @@
+// class Solution {
+//     public int[] finalPrices(int[] prices) {
+//         Stack<Integer> stack = new Stack();
+//         int n = prices.length;
+//         int[] ans = new int[n];
+//         for(int i=n-1; i>=0; i--){
+//             if(stack.isEmpty()) ans[i] = prices[i];
+            
+//             else if(stack.peek() > prices[i]) {
+//                 while(!stack.isEmpty() && stack.peek()>prices[i]) stack.pop();
+//                 if(stack.isEmpty()) ans[i] = prices[i];
+//                 else ans[i] = prices[i]-stack.peek();
+//             }
+//             else ans[i] = prices[i]-stack.peek();
+//             stack.push(prices[i]);
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int[] finalPrices(int[] prices) {
-        Stack<Integer> stack = new Stack();
         int n = prices.length;
         int[] ans = new int[n];
-        for(int i=n-1; i>=0; i--){
-            if(stack.isEmpty()) ans[i] = prices[i];
-            
-            else if(stack.peek() > prices[i]) {
-                while(!stack.isEmpty() && stack.peek()>prices[i]) stack.pop();
-                if(stack.isEmpty()) ans[i] = prices[i];
-                else ans[i] = prices[i]-stack.peek();
+        for(int i=0;i<n;i++)
+        {
+            ans[i] = prices[i];
+            for(int j=i+1;j<n;j++)
+            {
+                if(prices[j] <= prices[i])
+                {
+                    ans[i] = prices[i] - prices[j];
+                    break;
+                }
             }
-            else ans[i] = prices[i]-stack.peek();
-            stack.push(prices[i]);
         }
+        
         return ans;
     }
 }
