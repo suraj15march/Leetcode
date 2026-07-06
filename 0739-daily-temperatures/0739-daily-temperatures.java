9c71b43209ea066d4f6cedcf8d871e35012d63d8
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-07-06 21:03:47 +0530

    Time: 77 ms (26.16%), Space: 106.9 MB (50.62%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -4,16 +4,21 @@ class Solution {
         int n = temperatures.length;
         int[] ans = new int[n];
         for(int i= n-1; i>=0; i--){
-            if(stack.isEmpty()){
-                ans[i] = 0;
-            }
-            else if(temperatures[i] >= temperatures[stack.peek()]){
-                while(!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) stack.pop();
-                if(stack.isEmpty()) ans[i] = 0;
-                else ans[i] = stack.peek() - i;
-            }
-            else
-                ans[i] = stack.peek() -i;
+            // if(stack.isEmpty()){
+            //     ans[i] = 0;
+            // }
+            // else if(temperatures[i] >= temperatures[stack.peek()]){
+            //     while(!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) stack.pop();
+            //     if(stack.isEmpty()) ans[i] = 0;
+            //     else ans[i] = stack.peek() - i;
+            // }
+            // else
+            //     ans[i] = stack.peek() -i;
+
+            while(!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) stack.pop();
+            if(stack.isEmpty()) ans[i] = 0;
+            else ans[i] = stack.peek() - i;
+            
             stack.push(i);
         }
         return ans;
