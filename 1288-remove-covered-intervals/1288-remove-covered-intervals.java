4f9b1f3e6df218b94a1d3debb23f9936651ad397
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-07-07 15:42:53 +0530

    Time: 6 ms (92.96%), Space: 46.7 MB (24.35%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -1,24 +1,24 @@
+import java.util.Arrays;
+
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-        int n = intervals.length;
-        Arrays.sort(intervals, (a,b) ->{
-            if(a[0] == b[0]) return Integer.compare(b[1],a[1]);
-            return Integer.compare(a[0],b[0]);
-        });
-        int[][] stack = new int[n][2];
-        int size = 0;
-        for(int i=0; i<n; i++){
-            int[] temp1 = intervals[i];
-            if(size == 0){ 
-                stack[size++]=temp1;
-                continue;
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] == b[0]) {
+                return Integer.compare(b[1], a[1]);
             }
-            int[] temp2 = stack[size-1];
-            int s1 = temp1[0], e1 = temp1[1];
-            int s2 = temp2[0], e2 = temp2[1];
-            if((s2>=s1 && e1>=e2) || (s1>=s2 && e2>=e1)) continue;
-            stack[size++] = temp1;
+            return Integer.compare(a[0], b[0]);
+        });
+
+        int remainingCount = 0;
+        int maxEnd = 0;
+
+        for (int[] curr : intervals) {
+            if (curr[1] > maxEnd) {
+                remainingCount++;
+                maxEnd = curr[1];
+            }
         }
-        return size;
+
+        return remainingCount;
     }
 }
