84cd221918da52f352478192c32744762fe3a04d
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-07-12 16:00:24 +0530

    Time: 20 ms (27.52%), Space: 86.7 MB (6.12%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.java
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.java
@@ -1,23 +1,30 @@
 class ProductOfNumbers {
     List<Integer>list;
+    List<Integer>prefixProduct;
     int size;
 
     public ProductOfNumbers() {
         list = new ArrayList();
+        prefixProduct = new ArrayList();
         size = 0;
     }
     
     public void add(int num) {
         list.add(num);
+        if(prefixProduct.isEmpty()) prefixProduct.add(1);
+        if(num == 0) prefixProduct = new ArrayList();
+        else prefixProduct.add(prefixProduct.get(prefixProduct.size()-1)*num);
         size++;
     }
     
     public int getProduct(int k) {
-        int product = 1;
-        for(int i=size-1; i>=0 && i>=size-k; i--){
-            product *= list.get(i);
-        }
-        return product;
+        // int product = 1;
+        // for(int i=size-1; i>=0 && i>=size-k; i--){
+        //     product *= list.get(i);
+        // }
+        int n = prefixProduct.size();
+        if(k>=n) return 0;
+        return prefixProduct.get(n-1)/prefixProduct.get(n-k-1);
     }
 }
 
