8fdb3511f75e8156ec2480323370f373520729f4
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-07-18 01:26:37 +0530

    Time: 35 ms (81.03%), Space: 85.4 MB (73.61%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,21 +1,11 @@
 import java.util.*;
 class Solution {
     public int minSetSize(int[] arr) {
-        int n = arr.length, count=0, ans=0;
-        SortedMap<Integer, Integer>map = new TreeMap();
+        int n = arr.length, count=0;
+        Map<Integer, Integer>map = new HashMap();
         for(int i=0; i<n; i++){
             map.put(arr[i], map.getOrDefault(arr[i],0)+1);
         }
-        // PriorityQueue<Integer>pq = new PriorityQueue(Collections.reverseOrder());
-        // for(int value: map.values()){
-        //     pq.offer(value);
-        // }
-        // while(!pq.isEmpty()){
-        //     count += pq.poll();
-        //     ans++;
-        //     if(count>=n/2) return ans;
-        // }
-
         ArrayList<Integer>list = new ArrayList();
         for(int value: map.values()){
             list.add(value);
@@ -24,7 +14,6 @@ class Solution {
         int m = list.size();
         for(int i=m-1; i>=0; i--){
             count += list.get(i);
-            // ans++;
             if(count>=n/2) return m-i;
         }
         return 0;
