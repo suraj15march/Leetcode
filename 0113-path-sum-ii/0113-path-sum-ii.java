23d5188240d50a95a13075e7375fcd04c2b0e2c9
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-08-01 00:29:21 +0530

    Time: 1 ms (99.97%), Space: 45.6 MB (39.88%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -16,19 +16,16 @@
 class Solution {
     public void helper(TreeNode root, int targetSum, int sum, List<List<Integer>>ans, List<Integer>list) {
         if(root == null) return;
+        list.add(root.val);
         if(root.left == null && root.right == null && sum+root.val==targetSum){
-            list.add(root.val);
             ans.add(new ArrayList<>(list));
-            list.remove(list.size()-1);
             return;
         }
         if(root.left != null){
-            list.add(root.val);
             helper(root.left, targetSum, sum+root.val, ans, list);
             list.remove(list.size()-1);
         }
         if(root.right != null){
-            list.add(root.val);
             helper(root.right, targetSum, sum+root.val, ans, list);
             list.remove(list.size()-1);
         }
