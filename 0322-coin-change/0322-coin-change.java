32153952326969afa1d4b94f679d480f7776aa2a
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-08-07 00:15:41 +0530

    Time: 34 ms (14.17%), Space: 48 MB (5.08%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,21 +1,20 @@
 class Solution {
-    int dp[][];
-    public int helper(int[] coins, int index, int amount) {
+    public int helper(int[] coins, int index, int amount, int[][]dp) {
         if(amount == 0) return 0;
         if(amount<0 || index == coins.length) return (int) 1e9;
         if(dp[index][amount]!=-1) return dp[index][amount];
-        dp[index][amount] = Math.min(1+helper(coins, index, amount-coins[index]), helper(coins, index+1, amount));
+        dp[index][amount] = Math.min(1+helper(coins, index, amount-coins[index], dp), helper(coins, index+1, amount, dp));
         return dp[index][amount];
     }
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        dp = new int[n][amount+1];
+        int dp[][] = new int[n][amount+1];
         for(int i=0; i<n; i++){
             for(int j=0;j<=amount; j++){
                 dp[i][j] = -1;
             }
         }
-        int ans = helper(coins, 0, amount);
+        int ans = helper(coins, 0, amount, dp);
         return ans>=1e9? -1: ans;
     }
 }
