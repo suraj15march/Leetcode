07f797059c7df589102e6317e6969c05559072cd
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-08-13 00:03:32 +0530

    Time: 66 ms (74.64%), Space: 88.8 MB (74.81%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -3,8 +3,8 @@ class Solution {
         Map<Integer, Integer>map = new HashMap<>();
         int i=0, j=0, n = nums.length, ans=0;
         while(i<=j && j<n){
-            if(!map.containsKey(nums[j])) map.put(nums[j], 0);
-            map.put(nums[j], map.get(nums[j])+1);
+            // if(!map.containsKey(nums[j])) map.put(nums[j], 0);
+            map.put(nums[j], map.getOrDefault(nums[j], 0)+1);
             while(map.get(nums[j])>k && i<j){
                 map.put(nums[i], map.get(nums[i])-1);
                 i++;
