3fa4f414548f0633a57d2c16a7f437f2a58c60e6
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-08-16 22:41:21 +0530

    Time: 12 ms (97.01%), Space: 53.8 MB (35.94%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.java
@@ -0,0 +1,26 @@
+class MyHashSet {
+    boolean[] map = new boolean[1000001];
+
+    public MyHashSet() {
+    }
+    
+    public void add(int key) {
+        map[key] = true;
+    }
+    
+    public void remove(int key) {
+        map[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return map[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
