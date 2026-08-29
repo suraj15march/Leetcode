747b24831d5b99ebaf447aff54c51083002411c6
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-08-29 23:09:31 +0530

    Time: 2 ms (95.71%), Space: 48.6 MB (70.02%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,14 +2,14 @@ class Solution {
     int[] dp;
 
     int getOnes(int n) {
-        if(dp[n] != -1) return dp[n];
+        if (dp[n] != -1)
+            return dp[n];
         if (n == 0) {
             return 0;
         }
         if (n % 2 == 1) {
             dp[n] = 1 + getOnes(n / 2);
-        }
-        else{
+        } else {
             dp[n] = getOnes(n / 2);
         }
         return dp[n];
@@ -18,8 +18,16 @@ class Solution {
     public int[] countBits(int n) {
         dp = new int[n + 1];
         Arrays.fill(dp, -1);
-        for (int i = 0; i <= n; i++) {
-            dp[i] = getOnes(i);
+        dp[0] = 0;
+        if(n==0) return dp;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            // dp[i] = getOnes(i);
+            if (i % 2 == 1) {
+                dp[i] = 1 + dp[i / 2];
+            } else {
+                dp[i] = dp[i / 2];
+            }
         }
         return dp;
     }
