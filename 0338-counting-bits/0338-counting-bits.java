403250c5354f4495886ce14a2e8cb006a578279e
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-08-29 23:09:44 +0530

    Time: 3 ms (29.73%), Space: 48.6 MB (70.02%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -19,7 +19,9 @@ class Solution {
         dp = new int[n + 1];
         Arrays.fill(dp, -1);
         dp[0] = 0;
-        if(n==0) return dp;
+        if (n == 0) {
+            return dp;
+        }
         dp[1] = 1;
         for (int i = 2; i <= n; i++) {
             // dp[i] = getOnes(i);
