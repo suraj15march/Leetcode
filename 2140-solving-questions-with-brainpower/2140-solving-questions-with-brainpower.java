2540572284bebcea8ac82dc21776053a50406da7
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-09-16 00:06:12 +0530

    Time: 10 ms (50.56%), Space: 157.1 MB (20.64%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -0,0 +1,16 @@
+class Solution {
+    long helper(int index, int[][] questions, long[] dp){
+        if(index>=questions.length) return 0;
+        if(dp[index] != -1L) return dp[index];
+        long point = questions[index][0];
+        int brainpower = questions[index][1]+1;
+        long solve = point + helper(index+brainpower, questions, dp);
+        long skip = helper(index+1, questions, dp);
+        return dp[index] = Math.max(solve, skip);
+    }
+    public long mostPoints(int[][] questions) {
+        long[] dp = new long[questions.length];
+        Arrays.fill(dp, -1);
+        return helper(0, questions, dp);
+    }
+}
