6f7e0cdfc41fb20830beba894b0010a92a1a0691
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-09-15 20:24:44 +0530

    Time: 1 ms (85.43%), Space: 47.2 MB (58.61%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,28 +1,33 @@
 class Solution {
-    void dfs(int start, boolean[] visited, Map<Integer, List<Integer>> graph){
+    void dfs(int start, boolean[] visited, int[][] graph){
         if(visited[start]) return;
         visited[start] = true;
-        List<Integer>list = graph.get(start);
-        for(int i=0; i<list.size(); i++){
-            dfs(list.get(i), visited, graph);
+        // List<Integer>list = graph.get(start);
+        // for(int i=0; i<list.size(); i++){
+        //     dfs(list.get(i), visited, graph);
+        // }
+        for(int i=0; i<graph[start].length; i++){
+            if(graph[start][i] == 1)
+                dfs(i, visited, graph);
         }
     }
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(isConnected[i][j] == 1){
-                    graph.computeIfAbsent(i, (k -> new ArrayList<>())).add(j);
-                }
-            }
-        }
+        // Map<Integer, List<Integer>> graph = new HashMap<>();
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(isConnected[i][j] == 1){
+        //             graph.computeIfAbsent(i, (k -> new ArrayList<>())).add(j);
+        //         }
+        //     }
+        // }
         boolean[] visited = new boolean[n];
         int ans = 0;
         for(int i=0; i<n; i++){
             if(!visited[i]){
                 ans++;
-                dfs(i, visited, graph);
+                // dfs(i, visited, graph);
+                dfs(i, visited, isConnected);
             }
         }
         return ans;
