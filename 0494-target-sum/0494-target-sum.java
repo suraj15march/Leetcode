4345e3451bf13f992c27ff2400f034c600741f26
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2026-09-17 21:41:27 +0530

    Time: 4 ms (87.43%), Space: 45.1 MB (44.53%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,16 +1,21 @@
 class Solution {
-    int subsetSum(int index, int target, int[] nums){
+    int subsetSum(int index, int target, int[] nums, int[][] dp){
         if(index>=nums.length){
-            if(target == 0) return 1;
-            return 0;
+            return target==0? 1: 0;
         }
-        return subsetSum(index+1, target-nums[index], nums) + subsetSum(index+1, target, nums);
+        if(target<0) return 0;
+        if(dp[index][target] != -1) return dp[index][target];
+        return dp[index][target] = subsetSum(index+1, target-nums[index], nums, dp) + subsetSum(index+1, target, nums, dp);
     }
     public int findTargetSumWays(int[] nums, int target) {
         int sum = 0;
         for(int num: nums) sum += num;
-        if(target>sum || (sum+target)%2 !=0 ) return 0;
+        if(Math.abs(target)>sum || (sum+target)%2 !=0 ) return 0;
         int s1 = (sum+target)/2;
-        return subsetSum(0, s1, nums);
+        int[][] dp = new int[nums.length][s1+1];
+        for(int[] arr: dp){
+            Arrays.fill(arr, -1);
+        }
+        return subsetSum(0, s1, nums, dp);
     }
 }
